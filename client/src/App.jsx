37afc50efbdc587fd548089c187e01bb06a456d6
iampkumar02 <pkjjjjj123@gmail.com>
2022-04-15 13:04:33 +0530

    create react route pages and its implementaion

---
+++ client/src/App.jsx
@@ -0,0 +1,45 @@
+// import "./app.scss";
+import Home from "./pages/home/home";
+import Register from "./pages/register/register";
+import Watch from "./pages/watch/watch";
+import Login from "./pages/login/login";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./authContext/AuthContext";
+
+const App = () => {
+    const { user } = useContext(AuthContext);
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    {user ? <Home /> : <Redirect to="/register" />}
+                </Route>
+                <Route path="/register">
+                    {!user ? <Register /> : <Redirect to="/" />}
+                </Route>
+                <Route path="/login">{!user ? <Login /> : <Redirect to="/" />}</Route>
+                {user && (
+                    <>
+                        <Route path="/movies">
+                            <Home type="movie" />
+                        </Route>
+                        <Route path="/series">
+                            <Home type="series" />
+                        </Route>
+                        <Route path="/watch">
+                            <Watch />
+                        </Route>
+                    </>
+                )}
+            </Switch>
+        </Router>
+    );
+};
+
+export default App;
